8853255365a3cebe082f4637f2618b8042db3d40
zjl <jialuzhang805@gmail.com>
2021-07-18 22:51:37 +0800

    sync week02

--- Module 1/playground/src/components/TemplateEngine/engine2.js
+++ Module 1/playground/src/components/TemplateEngine/engine2.js
@@ -4,7 +4,7 @@ export default class Engine2 {
     render(tmpl, data) {
         const re1 = /<(\w+)\s*([^>]*)>([^<]*)<\/\1>/gm; //匹配<div class="a">XXX</div>
         const re2 = /<(\w+)\s*([^(/>)]*)\/>/gm; //匹配<img src="a"/>
-        tmpl = tmpl.replace(/\n/gm, "");
+        tmpl = tmpl.replace(/[\s|\n]/gm, "");
         while(re1.test(tmpl) || re2.test(tmpl)) {
             tmpl = tmpl.replace(re1, ($0, $1, $2, $3) => {
                 let attr = this.parseAttribute($2);
@@ -16,8 +16,8 @@ export default class Engine2 {
             //<img src="a"/>类型
             tmpl = tmpl.replace(re2, (s0, s1, s2) => {
                 let attr = this.parseAttribute(s2);
-                let node = new Vnode(s1, attr, [], null, "");
-                this.nodes.set(node.uuid, node);
+                let node = new VNode(s1, attr, [], null, "");
+                // this.nodes.set(node.uuid, node);
                 return `(${node.uuid})`;
             });
         }

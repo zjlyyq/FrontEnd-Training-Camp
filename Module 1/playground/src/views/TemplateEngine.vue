c759fe2afc62a896496c624fd20da18eb775cdfd
HIK\zhangjialu6 <zhangjialu6@hikvision.com>
2021-07-21 09:55:35 +0800

    sync

--- Module 1/playground/src/views/TemplateEngine.vue
+++ Module 1/playground/src/views/TemplateEngine.vue
@@ -9,7 +9,7 @@ import { V1} from '../components/TemplateEngine'
 export default {
     mounted() {
         const v1 = new V1().mounted(this.$el);
-        v1.render(`<div>hello {{name}}</div>`, {name: 'zjl'})
+        v1.render(`<div>hello {{name}}</div>`, {name: 'zjl'});
     }
 }
 </script>

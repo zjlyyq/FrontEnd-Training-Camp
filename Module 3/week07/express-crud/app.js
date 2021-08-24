56e131b92cac004b96b371cc9de3601426152a08
zjl <2868989685@qq.om>
2021-08-24 12:59:15 +0000

    sync week07

--- Module 3/week07/express-crud/app.js
+++ Module 3/week07/express-crud/app.js
@@ -1,40 +1,8 @@
 const path = require('path');
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
+const { getAllAnimals, getAllTodos, getAllUsers } = require('./plugins/mongo');
 
 const app = express();
-
-async function getAllTodos() {
-    // Connection URL
-    const url = 'mongodb://admin001:admin123456@localhost:27017/?authMechanism=SCRAM-SHA-256&authSource=todos';
-    const client = new MongoClient(url);
-    let dbName = "todos";
-    
-    // Use connect method to connect to the server
-    await client.connect()
-    console.log(`Connected successfully to db: ${dbName}`);
-    const db = client.db(dbName)
-    // db.aut
-    const collection = db.collection('todos')
-    let todos = await collection.find().toArray();
-    return todos;
-}
-
-async function getAllUsers() {
-    // Connection URL
-    const url = 'mongodb://root:password@localhost:27017/?authMechanism=SCRAM-SHA-256';
-    const client = new MongoClient(url);
-    let dbName = "admin";
-    
-    const app = express();
-    // Use connect method to connect to the server
-    await client.connect()
-    console.log(`Connected successfully to db: ${dbName}`);
-    const db = client.db(dbName)
-    const collection = db.collection('system.users')
-    let users = await collection.find().toArray();
-    return users;
-}
 app.get('/', (req, res) => {
     res.send('hello world');
 })
@@ -52,6 +20,15 @@ app.get('/doc', (req, res) => {
 app.get('/todos', async (req, res) => {
     try {
         let todos = await getAllTodos();
+        res.send(todos);
+    } catch (error) {
+        console.log(error)
+        res.send('no todos!');
+    }
+})
+app.get('/animals', async (req, res) => {
+    try {
+        let todos = await getAllAnimals();
         res.send(todos);
     } catch (error) {
         console.log(error)

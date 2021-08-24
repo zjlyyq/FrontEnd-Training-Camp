53bace7a6ba53bbe058559cd042603c5ae823dea
zjl <2868989685@qq.om>
2021-08-24 06:15:22 +0000

    用户名：密码连接需要授权的数据库需要使用authMechanism=xxx&authSource=xxx 参数的形式

--- Module 3/week07/express-crud/app.js
+++ Module 3/week07/express-crud/app.js
@@ -2,24 +2,39 @@ const path = require('path');
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
-const client = new MongoClient(url);
-let dbName = "todos";
-
 const app = express();
 
 async function getAllTodos() {
+    // Connection URL
+    const url = 'mongodb://admin001:admin123456@localhost:27017/?authMechanism=SCRAM-SHA-256&authSource=todos';
+    const client = new MongoClient(url);
+    let dbName = "todos";
+    
     // Use connect method to connect to the server
     await client.connect()
-    console.log('Connected successfully to server')
+    console.log(`Connected successfully to db: ${dbName}`);
     const db = client.db(dbName)
+    // db.aut
     const collection = db.collection('todos')
     let todos = await collection.find().toArray();
     return todos;
 }
 
-
+async function getAllUsers() {
+    // Connection URL
+    const url = 'mongodb://root:password@localhost:27017/?authMechanism=SCRAM-SHA-256';
+    const client = new MongoClient(url);
+    let dbName = "admin";
+    
+    const app = express();
+    // Use connect method to connect to the server
+    await client.connect()
+    console.log(`Connected successfully to db: ${dbName}`);
+    const db = client.db(dbName)
+    const collection = db.collection('system.users')
+    let users = await collection.find().toArray();
+    return users;
+}
 app.get('/', (req, res) => {
     res.send('hello world');
 })
@@ -39,7 +54,18 @@ app.get('/todos', async (req, res) => {
         let todos = await getAllTodos();
         res.send(todos);
     } catch (error) {
+        console.log(error)
         res.send('no todos!');
+    }
+})
+
+app.get('/users', async (req, res) => {
+    try {
+        let users = await getAllUsers();
+        res.send(users);
+    } catch (error) {
+        console.log(error)
+        res.send('no users!');
     }
 })
 

905aa381019b8179a066da1c1b2544989be7a670
zjl <jialuzhang0805@gmail.com>
2021-09-08 23:54:37 +0800

    实现了拖动时间demo，但还需要添加中间预览的部件和实现更多表单组件

--- Module 4/week09/App.vue
+++ Module 4/week09/App.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="container" style="display: flex; height: 100%;">
         <div class="pans" style="width: 30%; border: 1px solid #ccc;">
-            <button data-name="input" draggable="true">input</button>
-            <button data-name="checkbox" draggable="true">checkbox</button>
-            <button data-name="button" draggable="true">button</button>
-            <button data-name="image" draggable="true">image</button>
-            <button data-name="radio" draggable="true">input</button>
+            <button class="meta" data-name="input" draggable="true">input</button>
+            <button class="meta" data-name="checkbox" draggable="true">checkbox</button>
+            <button class="meta" data-name="button" draggable="true">button</button>
+            <button class="meta" data-name="image" draggable="true">image</button>
+            <button class="meta" data-name="radio" draggable="true">input</button>
         </div>
         <div class="preview" style="width: 40%;border: 1px solid #ccc;margin: 0 10px;"></div>
         <div style="flex: 1;border: 1px solid #ccc;padding: 0 5px;">
@@ -17,6 +17,51 @@
 <script>
 export default {
     name: "App",
+    mounted() {
+        document.querySelector('.pans').addEventListener('dragstart', e => {
+            console.log('dragstart', e);
+            e.dataTransfer.setData('name', 
+                e.target.getAttribute('data-name')
+            )
+            console.log(e.target.getAttribute('data-name'))
+        })
+        document.querySelector('.preview').addEventListener('dragenter', e => {
+            console.log('dragenter', e);
+            e.preventDefault();
+        })
+        document.querySelector('.preview').addEventListener('dragover', e => {
+            // 覆盖默认行为让元素可被放置
+            e.preventDefault();
+        })
+        document.querySelector('.preview').addEventListener('drop', e => {
+            console.log('drop', e);
+            // 覆盖默认行为让元素可被放置
+            e.preventDefault();
+            const type = e.dataTransfer.getData('name');
+            console.log(type)
+            switch(type) {
+                case 'inout':
+                    this.json.push(
+                        {
+                            name: 'input',
+                            type: 'text',
+                            placeholder: 'Enter Your Name...',
+                            value: ''
+                        }
+                    )
+                    break;
+                default:
+                    this.json.push(
+                        {
+                            name: 'input',
+                            type: 'text',
+                            placeholder: 'Enter Your Name...',
+                            value: ''
+                        }
+                    )
+            }
+        })
+    },
     data() {
         return {
             config: {
@@ -35,24 +80,24 @@ export default {
                 value: '123456'
             },
             json: [
-                {
-                    name: 'input',
-                    type: 'text',
-                    placeholder: 'Enter Your Name...',
-                    value: ''
-                },
-                {
-                    name: 'input',
-                    type: 'number',
-                    placeholder: 'Enter Your Age...',
-                    value: ''
-                },
-                {
-                    name: 'checkout',
-                    type: 'number',
-                    placeholder: 'Enter Your Age...',
-                    value: ''
-                },
+                // {
+                //     name: 'input',
+                //     type: 'text',
+                //     placeholder: 'Enter Your Name...',
+                //     value: ''
+                // },
+                // {
+                //     name: 'input',
+                //     type: 'number',
+                //     placeholder: 'Enter Your Age...',
+                //     value: ''
+                // },
+                // {
+                //     name: 'checkout',
+                //     type: 'number',
+                //     placeholder: 'Enter Your Age...',
+                //     value: ''
+                // },
             ]
         }
     },

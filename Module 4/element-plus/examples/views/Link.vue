470a6704801ceecc0b35e875aaa97da51a5e844c
zjl <jialuzhang805@gmail.com>
2021-10-18 02:19:49 +0800

    element-plus

---
+++ Module 4/element-plus/examples/views/Link.vue
@@ -0,0 +1,25 @@
+
+<template>
+    <div class="page-link">
+        <h2>Link 文字链接</h2>
+        <Link></Link>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, toRefs, computed, h } from "vue";
+import type { PropType } from 'vue';
+import { Link } from "@/ele";
+
+export default defineComponent({
+    name: 'PageLink',
+    components: { Link },
+    setup(props, { emit, slots }) {
+        return {
+        }
+    }
+});
+</script>
+
+<style lang="scss" scoped>
+</style>
